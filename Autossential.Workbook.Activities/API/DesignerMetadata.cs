3e5a467985fd63d6c488b3ac14b3bab9c2178d21
Alexandre <alexandretperez@gmail.com>
2026-06-28 17:54:31 -0300

    Code updates

---
+++ Autossential.Workbook.Activities/API/DesignerMetadata.cs
@@ -0,0 +1,17 @@
+﻿using UiPath.Studio.Activities.Api;
+
+namespace Autossential.Workbook.Activities.API
+{
+    public class DesignerMetadata : IRegisterWorkflowDesignApi
+    {
+        public void Initialize(IWorkflowDesignApi api)
+        {
+            api.ScopedActivitiesService.SetScopedActivities(typeof(WorkbookScope), [
+                typeof(ReadRange),
+                typeof(WriteRange),
+                typeof(ReadCell),
+                typeof(WriteCell)
+            ]);
+        }
+    }
+}
